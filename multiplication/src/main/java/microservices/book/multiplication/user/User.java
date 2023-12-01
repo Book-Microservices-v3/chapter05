bcfd007b37d8f74f35cd0a9fa7195a6017c55c45
Tarun Telang <tarun.telang@gmail.com>
2023-12-01 14:19:28 +0530

    Update User.java

--- multiplication/src/main/java/microservices/book/multiplication/user/User.java
+++ multiplication/src/main/java/microservices/book/multiplication/user/User.java
@@ -1,8 +1,21 @@
 package microservices.book.multiplication.user;
 
+import lombok.*;
+import jakarta.persistence.*;
 /**
- * Stores information to identify the user.
- */
-public record User(Long id, String alias) {
+* Stores information to identify the user.
+*/
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Users {
+ @Id
+ @GeneratedValue
+ private Long id;
+ private String alias;
+ public Users(final String userAlias) {
+ this(null, userAlias);
+ }
 }
 
