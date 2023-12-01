c23b2e4d61e8dfffd829d7a09a48ef08fd11854b
Tarun Telang <tarun.telang@gmail.com>
2023-12-01 14:42:16 +0530

    Update ChallengeServiceTest.java

--- multiplication/src/test/java/microservices/book/multiplication/challenge/ChallengeServiceTest.java
+++ multiplication/src/test/java/microservices/book/multiplication/challenge/ChallengeServiceTest.java
@@ -9,9 +9,20 @@ public class ChallengeServiceTest {
 
     private ChallengeService challengeService;
 
+    @Mock
+    private UserRepository userRepository;
+    
+    @Mock
+    private ChallengeAttemptRepository attemptRepository;
+
     @BeforeEach
     public void setUp() {
-        challengeService = new ChallengeServiceImpl();
+        challengeService = new ChallengeServiceImpl(
+             userRepository,
+             attemptRepository
+        );
+        given(attemptRepository.save(any()))
+             .will(returnsFirstArg());
     }
 
     @Test
@@ -26,6 +37,10 @@ public class ChallengeServiceTest {
 
         // then
         then(resultAttempt.correct()).isTrue();
+
+        // newly added lines
+        verify(userRepository).save(new User("john_doe"));
+        verify(attemptRepository).save(resultAttempt);
     }
 
     @Test
@@ -40,5 +55,27 @@ public class ChallengeServiceTest {
 
         // then
         then(resultAttempt.correct()).isFalse();
+    }
+
+    @Test
+    public void checkExistingUserTest() {
+        // given
+        given(attemptRepository.save(any()))
+                .will(returnsFirstArg());
+        User existingUser = new User(1L, "john_doe");
+        given(userRepository.findByAlias("john_doe"))
+                .willReturn(Optional.of(existingUser));
+        ChallengeAttemptDTO attemptDTO =
+                new ChallengeAttemptDTO(50, 60, "john_doe", 5000);
+
+        // when
+        ChallengeAttempt resultAttempt =
+                challengeService.verifyAttempt(attemptDTO);
+
+        // then
+        then(resultAttempt.isCorrect()).isFalse();
+        then(resultAttempt.getUser()).isEqualTo(existingUser);
+        verify(userRepository, never()).save(any());
+        verify(attemptRepository).save(resultAttempt);
     }
 }
